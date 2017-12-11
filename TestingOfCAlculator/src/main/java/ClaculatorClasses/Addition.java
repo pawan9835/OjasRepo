45b19ade784a8c9237b6a5902197ad898236fd4c
Pawank1994 <pawank1994r@gmail.com>
2017-12-11 16:18:23 +0530

    add comment

--- TestingOfCAlculator/src/main/java/ClaculatorClasses/Addition.java
+++ TestingOfCAlculator/src/main/java/ClaculatorClasses/Addition.java
@@ -6,6 +6,8 @@ import com.app.cal.ICalclator;
 public class Addition extends CalculatorOption{
 public double calculate(double valueA, double valueB) {
 		
+		
+		//durga
 		return valueA+valueB;
 
 }
