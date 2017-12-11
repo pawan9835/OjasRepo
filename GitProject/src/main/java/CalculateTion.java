2d4c75a421d571ce4b93cfc2f67668fbab97403c
Pawank1994 <pawank1994r@gmail.com>
2017-12-11 15:37:57 +0530

    added substract method

--- GitProject/src/main/java/CalculateTion.java
+++ GitProject/src/main/java/CalculateTion.java
@@ -7,12 +7,19 @@ public class CalculateTion {
 
 	}
 
+	
+	public void substract(int a,int b){
+		
+		
+		return a-b;
+	}
 	public static void main(String[] args) {
 		CalculateTion ob = new CalculateTion();
 
 		int r = ob.add(4, 5);
 
 		System.out.println(r);
+		System.out.println(ob.substract(14, 5));
 
 	}
 
