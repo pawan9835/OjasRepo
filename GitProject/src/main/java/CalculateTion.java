c1c97e618d75b44665346e232a093f6f3d318e00
Pawank1994 <pawank1994r@gmail.com>
2017-12-11 15:34:14 +0530

    Added one project

---
+++ GitProject/src/main/java/CalculateTion.java
@@ -0,0 +1,19 @@
+
+public class CalculateTion {
+
+	public int add(int a, int b) {
+
+		return a + b;
+
+	}
+
+	public static void main(String[] args) {
+		CalculateTion ob = new CalculateTion();
+
+		int r = ob.add(4, 5);
+
+		System.out.println(r);
+
+	}
+
+}
